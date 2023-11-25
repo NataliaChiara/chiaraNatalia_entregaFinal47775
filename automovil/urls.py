d5ad07dc56892ce16de462c8f42fc0dbc0dfd151
Natalia Chiara <natalia@contenidos-digitales.com>
2023-11-24 23:33:34 -0300

    .

--- automovil/urls.py
+++ automovil/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from automovil.views import ListadoBlogs, CrearBlog, ActualizarBlog, EliminarBlog, DetalleBlog
 
 urlpatterns = [
-    path('automoviles/', ListadoBlogs.as_view(), name='blogs'),
-    path('automoviles/crear', CrearBlog.as_view(), name='crear_blog'),
-    path('automoviles/<int:pk>', DetalleBlog.as_view(), name='detalle_blog'),
-    path('automoviles/<int:pk>/eliminar', EliminarBlog.as_view(), name='eliminar_blog'),
-    path('automoviles/<int:pk>/actualizar', ActualizarBlog.as_view(), name='actualizar_blog')
+    path('blogs/', ListadoBlogs.as_view(), name='blogs'),
+    path('blogs/crear', CrearBlog.as_view(), name='crear_blog'),
+    path('blogs/<int:pk>', DetalleBlog.as_view(), name='detalle_blog'),
+    path('blogs/<int:pk>/eliminar', EliminarBlog.as_view(), name='eliminar_blog'),
+    path('blogs/<int:pk>/actualizar', ActualizarBlog.as_view(), name='actualizar_blog')
 ]
