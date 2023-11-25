7f74a5bc9ad1b3c9a823511f5123e333a9278c07
Natalia Chiara <natalia@contenidos-digitales.com>
2023-11-24 23:29:50 -0300

    otro cambio chiquito asi no rompemos nada

--- automovil/urls.py
+++ automovil/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from automovil.views import ListadoAutomoviles, CrearAutomovil, ActualizarAutomovil, EliminarAutomovil, DetalleAutomovil
+from automovil.views import ListadoBlogs, CrearBlog, ActualizarBlog, EliminarBlog, DetalleBlog
 
 urlpatterns = [
-    path('automoviles/', ListadoAutomoviles.as_view(), name='blogs'),
-    path('automoviles/crear', CrearAutomovil.as_view(), name='crear_blog'),
-    path('automoviles/<int:pk>', DetalleAutomovil.as_view(), name='detalle_blog'),
-    path('automoviles/<int:pk>/eliminar', EliminarAutomovil.as_view(), name='eliminar_blog'),
-    path('automoviles/<int:pk>/actualizar', ActualizarAutomovil.as_view(), name='actualizar_blog')
+    path('automoviles/', ListadoBlogs.as_view(), name='blogs'),
+    path('automoviles/crear', CrearBlog.as_view(), name='crear_blog'),
+    path('automoviles/<int:pk>', DetalleBlog.as_view(), name='detalle_blog'),
+    path('automoviles/<int:pk>/eliminar', EliminarBlog.as_view(), name='eliminar_blog'),
+    path('automoviles/<int:pk>/actualizar', ActualizarBlog.as_view(), name='actualizar_blog')
 ]
--- automovil/views.py
+++ automovil/views.py
@@ -6,28 +6,28 @@ from automovil.models import Blog
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-class ListadoAutomoviles(ListView):
+class ListadoBlogs(ListView):
     model = Blog
     context_object_name = 'listado_de_blogs'
     template_name = 'blog/blogs.html'
 
-class CrearAutomovil(LoginRequiredMixin, CreateView):
+class CrearBlog(LoginRequiredMixin, CreateView):
     model = Blog
     template_name = 'blog/crear_blog.html'
     fields= ['titulo', 'autor', 'descripcion', 'fecha_creacion']
     success_url = reverse_lazy('blogs')
 
-class ActualizarAutomovil(LoginRequiredMixin, UpdateView):
+class ActualizarBlog(LoginRequiredMixin, UpdateView):
     model = Blog
     template_name = 'blog/actualizar_blog.html'
     fields= ['titulo', 'autor', 'descripcion', 'fecha_creacion']
     success_url = reverse_lazy('blogs')
 
-class DetalleAutomovil(DetailView):
+class DetalleBlog(DetailView):
     model = Blog
     template_name = 'blog/detalle_blog.html'
 
-class EliminarAutomovil(LoginRequiredMixin, DeleteView):
+class EliminarBlog(LoginRequiredMixin, DeleteView):
     model = Blog
     template_name = 'blog/eliminar_blog.html'
     success_url = reverse_lazy('blogs')
