28012972dea67e81dc9ba34130cd014804ac2107
Natalia Chiara <natalia@contenidos-digitales.com>
2023-12-15 16:22:51 -0300

    acerca

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from inicio.views import inicio
+from inicio.views import inicio, acerca
 
 urlpatterns = [
-    path('', inicio, name='inicio')
+    path('', inicio, name='inicio'),
+    path('acerca/', acerca, name='acerca')
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'inicio/inicio.html', {})
+
+def acerca(request):
+    return render(request, 'inicio/acerca.html', {})
